3311a9640c15a297bf8bf450fece393b8fe83e8f
maskhav <60893132+maks331122@users.noreply.github.com>
2022-12-13 23:11:46 +0200

    version1_test
    tested version

--- bot.py.py
+++ bot.py.py
@@ -3,29 +3,26 @@ from telebot import types
 
 
 
-phone_book = {
-    'Бабурнич': +380951321834,
-    'Боровський': +380968533100,
-    'Борщун': +380953648652, 
+phone_book = {                                                              #Номера телефону групи
+    'бабурнич': +380951321834,              
+    'боровський': +380968533100,            
+    'борщун': +380953648652,                
 }
 
-bot = telebot.TeleBot('5780863217:AAG7-Zq0_yB5dBzgUHclz-QFPZ8-E8FCh4Q')
-@bot.message_handler(commands=['start'])
+bot = telebot.TeleBot('5780863217:AAG7-Zq0_yB5dBzgUHclz-QFPZ8-E8FCh4Q')     #Токен бота
+
+@bot.message_handler(commands=['start'])                                    #Команда /start
 def start(message):
     mess = f'Hello, {message.from_user.first_name}'
-    bot.send_message(message.chat.id, mess)
-
-@bot.message_handler(commands=['help'])
-def help(message):
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-    start = types.KeyboardButton('/start')
-    phone_number = types.KeyboardButton('Боровський')
-    markup.add(start, phone_number)
-    bot.send_message(message.chat.id, 'Choose action: ', reply_markup=markup)
+    userid = types.KeyboardButton('Get ID')
+    get_phone = types.KeyboardButton('Phone numbers')
+    markup.add(userid, get_phone);
+    bot.send_message(message.chat.id, mess, reply_markup=markup)
 
-@bot.message_handler(commands=['get_phone_number'])
-def help(message):
+def numbers(message):                                                       #Фамілії групи 
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=3)
+    menu = types.KeyboardButton('Головне меню')
     baburnich = types.KeyboardButton('Бабурнич')
     borovskiy = types.KeyboardButton('Боровський')
     borshchun = types.KeyboardButton('Борщун')
@@ -54,16 +51,27 @@ def help(message):
     startseva = types.KeyboardButton('Старцева')
     tsumbaliuk = types.KeyboardButton('Цимбалюк')
     shpuchka = types.KeyboardButton('Шпичка')
-    markup.add(baburnich, borovskiy, borshchun, vasulchenko, voznuy, gorbatiuk, grinchuk, derkach, kozik, kompaniets, ksendzuk,
+    markup.add(menu, baburnich, borovskiy, borshchun, vasulchenko, voznuy, gorbatiuk, grinchuk, derkach, kozik, kompaniets, ksendzuk,
                marko, marchenkoa, marchenkoo, obuhivskiy, ognivenko, oliynuchenko, parhomenko, pruimuch,
                protasov, romaniuk, salik, safonov, safronov, sorokotiaga, startseva, tsumbaliuk, shpuchka)
     bot.send_message(message.chat.id, 'Choose action: ', reply_markup=markup)
 
-@bot.message_handler(content_types=['text'])
-def get_phone_number(message):
-    last_name = message.text
-    number = phone_book.get(last_name)
-    bot.send_message(message.chat.id, str(number))
+@bot.message_handler(content_types=['text'])                                #Обробка запитів
+def read_user_text(message):
+    if(message.text.lower() == 'головне меню'):
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+        userid = types.KeyboardButton('Get ID')
+        get_phone = types.KeyboardButton('Phone numbers')
+        markup.add(userid, get_phone)
+        bot.send_message(message.chat.id,'Choose action: ', reply_markup=markup)
+    if(message.text.lower() == 'get id'):
+        bot.send_message(message.chat.id, f'{message.from_user.first_name}:{message.from_user.id}')
+    if(message.text.lower() == 'phone numbers'):
+        numbers(message);
+    try:
+        if(phone_book.fromkeys(message.text.lower())):
+            bot.send_message(message.chat.id, phone_book.get(message.text.lower()))
+    except:
+        pass
 
-
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, interval=0)                                     #Багатопоточність
